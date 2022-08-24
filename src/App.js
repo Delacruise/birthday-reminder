d64c12d210e9e560b99e4f64bf3105cea0101e67
Luke Daffue <25479590+Delacruise@users.noreply.github.com>
2022-08-24 17:50:31 +0400

    New version with individual remove options

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React, { useState } from 'react';
-import './App.css';
-import TheList from './dataList';
-import data from './data';
 
-const App = () => {
+import './App.css';
+import data from './data';
+import List from './dataList';
+
+export const App = () => {
   const [people, setPeople] = useState(data);
 
-  function clearAll() {
+  const clearList = () => {
     setPeople([]);
-  }
-
-  function birthdayPlural() {
-    if (people.length === 1) {
-      return `There is 1 birthday today.`;
-    } else if (people.length === 0) {
-      return `There are no birthdays today.`;
-    } else {
-      return `There are ${people.length} birthdays today.`;
-    }
-  }
+  };
 
   return (
-    <>
-      <section className='container'>
-        <header>
-          <h3>{birthdayPlural()}</h3>
-        </header>
-        <TheList people={people} />
-        <button className='btn' onClick={() => clearAll()}>
-          Clear All
-        </button>
-      </section>
-    </>
+    <main>
+      <List />
+    </main>
   );
 };
-
 export default App;
--- src/dataList.js
+++ src/dataList.js
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
-const TheList = ({ people }) => {
+const List = () => {
+  const [people, setPeople] = useState(data);
+
+  function removeFromList({ id }) {
+    console.log('We are in ' + id);
+    let updatedPeopleList = people.filter((person) => person.id !== id);
+    setPeople(updatedPeopleList);
+  }
+
+  const clearList = () => {
+    setPeople([]);
+  };
   return (
     <>
-      {people.map((person) => {
-        const { id, name, age, image } = person;
-        return (
-          <article key={id}>
-            <div className='birtdayBlock'>
-              <img className='thePic' src={image}></img>
-              <div className='theName'>{name}</div>
-              <div className='theAge'>{age} years</div>
-            </div>
-          </article>
-        );
-      })}
+      <header>
+        <h3>
+          Today there are {people.length} people celebrating their birthdays.
+        </h3>
+      </header>
+      <section>
+        {people.map((person) => {
+          const { id, name, age, image } = person;
+          return (
+            <article key={id}>
+              <img className='personPic' src={image} alt={name} />
+
+              <div className='personDetails'>
+                <div className='personName'>{name}</div>
+                <div className='personAge'>{age}</div>
+              </div>
+              <div className='removePerson'>
+                <button className='btn' onClick={() => removeFromList({ id })}>
+                  Congratulated
+                </button>
+              </div>
+            </article>
+          );
+        })}
+      </section>
+      <footer>
+        <button className='btn' onClick={() => clearList()}>
+          Congratualated All
+        </button>
+      </footer>
     </>
   );
 };
 
-export default TheList;
+export default List;
